213b8876ba464be86ee269d23b2c2244e0e5b2ae
Grant Gorham <grantegorham26@gmail.com>
2024-05-20 19:15:15 -0600

    added legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,6 +10,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
 
+
 // earthquake url
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson';
 
@@ -34,7 +35,7 @@ function createFeatures(earthquakeData){
         };
         return L.circleMarker(latlng, markerCustom)
     }
-    // usea  color scale for color of each marker
+    // use a color scale for color of each marker
     let colorScale = d3.scaleSequential()
     // range of earthquake depth
     .domain([0,100])
@@ -50,3 +51,25 @@ function createFeatures(earthquakeData){
     
     }).addTo(myMap);
 }
+
+// create color scale for legend same as marker color
+let colorScale = d3.scaleSequential()
+.domain([0,100])
+.interpolator(d3.interpolateYlGnBu);
+// create legend
+let legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    let depthRange = [0,20,40,60,80,100];
+
+    // Loop through depth ranges to create legend items
+    for (let i = 0; i < depthRange.length - 1; i++) {
+        let rangeStart = depthRange[i];
+        let rangeEnd = depthRange[i + 1];
+        let color = colorScale((rangeStart + rangeEnd) / 2);
+        let rangeText = (rangeEnd === 100) ? `${rangeStart}+ km` : `${rangeStart} - ${rangeEnd} km`;
+        div.innerHTML += `<i style="background: ${color}"></i> ${rangeText} <br>`;
+    }
+return div;
+};
+legend.addTo(myMap);
